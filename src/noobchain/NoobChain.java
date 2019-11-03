27d7dc50a90677950b319782b39b767302fdd2e3
vaishnavi1401 <vaishnaviaggarwal1401@gmail.com>
2019-11-04 00:03:29 +0530

    ADD

---
+++ src/noobchain/NoobChain.java
@@ -0,0 +1,63 @@
+package noobchain;
+import java.util.ArrayList;
+import com.google.gson.GsonBuilder;
+import java.util.*;
+public class NoobChain {
+	static Scanner so= new Scanner(System.in);
+	public static ArrayList<Block> blockchain = new ArrayList<Block>();
+	public static int difficulty = 5;
+
+	public static void main(String[] args) {	
+		//add our blocks to the blockchain ArrayList:
+		int i=1;
+		String s,s1;
+		System.out.println("Enter data into blockxchain ");
+		s=so.next();
+		addBlock(new Block(s, "0"));
+		do {
+		System.out.println("Trying to Mine block 2... ");
+		s1=so.next();
+		addBlock(new Block(s1,blockchain.get(blockchain.size()-1).hash));
+		i=so.nextInt();
+		}while(i!=0);
+		System.out.println("\nBlockchain is Valid: " + isChainValid());
+		
+		String blockchainJson = StringUtil.getJson(blockchain);
+		System.out.println("\nThe block chain: ");
+		System.out.println(blockchainJson);
+	}
+	
+	public static Boolean isChainValid() {
+		Block currentBlock; 
+		Block previousBlock;
+		String hashTarget = new String(new char[difficulty]).replace('\0', '0');
+		
+		//loop through blockchain to check hashes:
+		for(int i=1; i < blockchain.size(); i++) {
+			currentBlock = blockchain.get(i);
+			previousBlock = blockchain.get(i-1);
+			//compare registered hash and calculated hash:
+			if(!currentBlock.hash.equals(currentBlock.calculateHash()) ){
+				System.out.println("Current Hashes not equal");			
+				return false;
+			}
+			//compare previous hash and registered previous hash
+			if(!previousBlock.hash.equals(currentBlock.previousHash) ) {
+				System.out.println("Previous Hashes not equal");
+				return false;
+			}
+			//check if hash is solved
+			if(!currentBlock.hash.substring( 0, difficulty).equals(hashTarget)) {
+				System.out.println("This block hasn't been mined");
+				return false;
+			}
+			
+		}
+		return true;
+	}
+	
+	public static void addBlock(Block newBlock) {
+		newBlock.mineBlock(difficulty);
+		blockchain.add(newBlock);
+	}
+}
